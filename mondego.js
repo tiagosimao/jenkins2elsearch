16ffa7fb59a5ac7299fe1bee600ef635c3066068
Tiago Simão <tiagosimao@gmail.com>
2017-05-14 11:02:21 +0100

    refactored

--- mondego.js
+++ mondego.js
@@ -1,25 +1,26 @@
-console.log("usage: node mondego.js <user> <pass>")
-
+const fs = require('fs');
 const http = require('https');
 const url = require('url');
+const request = require('request');
 const elasticsearch = require('elasticsearch');
+const events = require('events');
+const bus = new events.EventEmitter();
 
-//var parser = new xml2js.Parser();
-var elclient = new elasticsearch.Client({
-    host: 'localhost:9200',
-    log: 'debug'
-});
+const moduleReadyEvent = "moduleReadyEvent";
 
-var elindex = "projects";
-var jenkinsProtocol = "https:";
-var jenkinsHost = "ci.impresa.pt";
+let writerReady = false;
+let queuedReaders = [];
 
-function getJson(protocol, host, path, reader) {
+let config;
+let elclient;
+
+// HTTP TOOLS
+function getJson(protocol, host, path, user, pass, reader) {
     var options = {
         protocol: protocol,
         hostname: host,
         path: path,
-        auth: process.argv[2] + ":" + process.argv[3]
+        auth: user + ":" + pass
     }
     console.log("GET: " + path);
 
@@ -27,7 +28,7 @@ function getJson(protocol, host, path, reader) {
         res.setEncoding('utf8');
         var rawData = '';
         if(res.statusCode!=200) {
-            console.log("Error fetching " + path);
+            console.log("Error fetching " + path + " (" + res.statusCode + ")");
             return;
         }
         console.log("GOT: " + res.statusCode + " from " + path);
@@ -39,59 +40,190 @@ function getJson(protocol, host, path, reader) {
     });
 }
 
+// QUEUE
+function queue(reader) {
+    queuedReaders.push(reader);
+    bus.emit(moduleReadyEvent);
+}
+
+// READERS
+function sync() {
+    if(writerReady) {
+        let reader;
+        while(reader=queuedReaders.pop()) {
+            reader();
+        }
+    }
+}
+
 function syncBuilds(job,onSync) {
     for (var i in job.builds) {
         var build = job.builds[i];
         var buildUrl = url.parse(build.url);
         console.log("Found build " + build.number + " at " + build.url);
-        getJson(buildUrl.protocol, buildUrl.host, buildUrl.path + "api/json", function (buildData) {
-            submit(job.name + "", buildData.id + "", buildData);
+        getJson(buildUrl.protocol, buildUrl.host, buildUrl.path + "api/json",
+            config.jenkins.username, config.jenkins.password,
+            function (jenkinsBuildData) {
+                let culprit;
+                if(jenkinsBuildData.culprits && jenkinsBuildData.culprits.length > 0){
+                    culprit = jenkinsBuildData.culprits[0].fullName;
+                }
+                let buildData = {
+                    "id": jenkinsBuildData.id,
+                    "project": job.name,
+                    "duration": jenkinsBuildData.duration,
+                    "timestamp": new Date(jenkinsBuildData.timestamp).toISOString(),
+                    "status": jenkinsBuildData.result,
+                    "cause": jenkinsBuildData.description,
+                    "url": jenkinsBuildData.url,
+                    "user": culprit
+                };
+                write(config.elasticsearch.buildindex, job.name, buildData.id + "", buildData)
         });
     }
     onSync();
 }
 
 
-function syncJobs(jobs) {
-    if(!jobs) {
-        console.log("Taking our jobs");
-        getJson(jenkinsProtocol, jenkinsHost, "/jenkins/api/json", function (data) {
-            syncJobs(data.jobs);
-        });
-    } else {
+function syncJenkinsJobs(jobs) {
+    if (jobs) {
         var job = jobs.shift();
-        if(job) {
-            setMappings(elindex,job.name);
+        if (job) {
+            //setTimestampMapping(config.elasticsearch.buildindex, job.name, "timestamp");
             var jobUrl = url.parse(job.url);
             console.log("Found job " + job.name + " at " + job.url);
-            getJson(jobUrl.protocol, jobUrl.host, jobUrl.path + "api/json", function (data) {
-                syncBuilds(data,function(){syncJobs(jobs);});
-            });
+            getJson(jobUrl.protocol, jobUrl.host, jobUrl.path + "api/json",
+                config.jenkins.username, config.jenkins.password,
+                function (data) {
+                    syncBuilds(data, function () {
+                        syncJenkinsJobs(jobs);
+                    });
+                });
         }
+    } else {
+        console.log("Taking our jobs");
+        getJson(config.jenkins.protocol, config.jenkins.host, config.jenkins.path + "/api/json",
+            config.jenkins.username, config.jenkins.password,
+            function (data) {
+                syncJenkinsJobs(data.jobs);
+            });
     }
 }
 
+function syncGitlabProject(project) {
 
-function submit(jobName, buildNumber, data) {
-    data.jobName=jobName;
+}
+
+function syncGitlabProjects() {
+    var options = {
+        url: config.gitlab.url + "/projects/",
+        headers: {
+            'PRIVATE-TOKEN': config.gitlab.private_token
+        }
+    };
+    request(options, function(error, response, body){
+        if(response.statusCode<300){
+            let got = JSON.parse(body);
+            syncGitlabProject(got);
+        } else {
+            console.log("Error getting projects from Gitlab: " + response.statusCode)
+        }
+    });
+}
+
+// WRITE DATA
+
+function write(index, type, id, data) {
+    console.log("trying to write");
     elclient.index({
-        index: elindex,
-        type: jobName,
-        id: buildNumber,
+        index: index,
+        type: type,
+        id: id,
         body: data
     });
 }
 
-syncJobs();
-
-
-function setMappings(idx,t){
+/*function setTimestampMapping(index,type,property){
     var body = {};
-    body[t]= {
-        properties:{
-            timestamp: {"type" : "date"}
-        }
+    body[type]= {
+        properties:{}
     };
-    elclient.indices.putMapping({index:idx, type:t, body:body});
+    body[type].properties[property]={"type" : "date"};
+    elclient.indices.putMapping({index:index, type:type, body:body});
+}*/
+
+
+// SETUP MODULES
+
+function setup() {
+    setupJenkins();
+    setupGitlab();
+    setupElasticsearch();
 }
 
+function setupElasticsearch() {
+    elclient = new elasticsearch.Client({
+        host: config.elasticsearch.url,
+        log: 'info'
+    });
+    elclient.ping({
+        requestTimeout: 1000
+    }, function (error) {
+        if (error) {
+            console.error("Error connecting to Elasticsearch");
+        } else {
+            writerReady=true;
+            bus.emit(moduleReadyEvent);
+        }
+    });
+}
+
+function setupJenkins() {
+    let jurl = url.parse(config.jenkins.url);
+    if(jurl){
+        config.jenkins.protocol=jurl.protocol;
+        config.jenkins.host=jurl.host;
+        config.jenkins.path=jurl.path;
+    }
+    queue(syncJenkinsJobs);
+}
+
+function setupGitlab() {
+    request.post(
+        config.gitlab.url + "/session?login="+config.gitlab.username+"&password=" + config.gitlab.password,
+        function (error, response, body) {
+            if (!error && response.statusCode < 300) {
+                let got = JSON.parse(body);
+                config.gitlab.private_token = got.private_token;
+                if(config.gitlab.private_token) {
+                    console.log("Gitlab: Authenticated");
+                    queue(syncGitlabProjects);
+                } else {
+                    console.log("Gitlab: Invalid authentication response");
+                }
+            } else {
+                console.log("Gitlab: setup error: " + response.statusCode);
+            }
+        }
+    );
+}
+
+// BOOTSTRAP
+function loadConfig(configLocation) {
+    config = JSON.parse(fs.readFileSync(configLocation, encoding="utf-8"));
+}
+
+function boot() {
+    let configLocation = process.argv[2];
+    if(!configLocation) {
+        console.log("usage: node mondego.js <config file location>")
+    } else {
+        loadConfig(configLocation);
+        bus.on(moduleReadyEvent,()=>{
+            sync();
+        });
+        setup();
+
+    }
+}
+boot();
