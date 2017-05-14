72d4667b85b62587557ff321f57854fd3addcc14
Tiago Simão <tiagosimao@gmail.com>
2017-05-14 18:31:41 +0100

    jenkins promotion plugin

--- mondego.js
+++ mondego.js
@@ -7,6 +7,7 @@ const events = require('events');
 const bus = new events.EventEmitter();
 
 const moduleReadyEvent = "moduleReadyEvent";
+const jenkinsReadyEvent = "jenkinsReadyEvent";
 
 let writerReady = false;
 let queuedReaders = [];
@@ -14,31 +15,6 @@ let queuedReaders = [];
 let config;
 let elclient;
 
-// HTTP TOOLS
-function getJson(protocol, host, path, user, pass, reader) {
-    var options = {
-        protocol: protocol,
-        hostname: host,
-        path: path,
-        auth: user + ":" + pass
-    }
-    console.log("GET: " + path);
-
-    http.get(options, function(res) {
-        res.setEncoding('utf8');
-        var rawData = '';
-        if(res.statusCode!=200) {
-            console.log("Error fetching " + path + " (" + res.statusCode + ")");
-            return;
-        }
-        console.log("GOT: " + res.statusCode + " from " + path);
-        res.on('data', function(chunk) {rawData += chunk;});
-        res.on('end', function() {
-            var data = JSON.parse(rawData);
-            reader(data);
-        });
-    });
-}
 
 // QUEUE
 function queue(reader) {
@@ -58,13 +34,90 @@ function sync() {
 
 
 // JENKINS
+let jenkinsQueue = [];
+let jenkinsErrored = [];
+
+function readJenkinsQueue() {
+    let call;
+    let timeout = 100;
+    if(call=jenkinsQueue.pop()) {
+        call(call);
+    }
+    if(jenkinsErrored.length>0){
+        timeout=Math.min(100,Math.min(10000,jenkinsErrored.length*100));
+        jenkinsQueue.push(jenkinsErrored.pop());
+    }
+    setTimeout(readJenkinsQueue,timeout);
+}
+
+function jenkinsGet(path, reader) {
+    jenkinsQueue.push((myself)=>{
+        let callUri = url.resolve(config.jenkins.url,path);
+        console.info("Calling Jenkins: " + path);
+        request(url.format(callUri), (error,response,body)=>{
+            if(error){
+                console.error("Error getting data from Jenkins: " + callUri.path);
+                console.error(error);
+                if(!response||response.statusCode>499){
+                    jenkinsErrored.push(myself);
+                }
+            } else if(response && response.statusCode < 300) {
+                let data = JSON.parse(body);
+                reader(data);
+                return;
+            } else {
+                let cause = response ? response.statusCode : "unknown cause";
+                console.error("Error getting data from Jenkins: " + cause + " at " + path);
+            }
+        });
+    });
+}
+
+function syncPromotion(promotion,process,job,buildId) {
+    var promotionUrl = url.parse(promotion.url);
+    jenkinsGet(promotionUrl.path + "api/json",
+        got=>{
+            let promotionData = {
+                "id": got.id,
+                "repoName": job.name,
+                "duration": got.duration,
+                "created_timestamp": new Date(got.timestamp).toISOString(),
+                "status": got.result,
+                "description": got.description,
+                "url": got.url
+            };
+            write(config.elasticsearch.index, "release", promotionData.id + "", promotionData);
+        }
+    );
+}
+
+function syncProcess(process,job,buildId) {
+    var processUrl = url.parse(process.url);
+    jenkinsGet(processUrl.path + "api/json",
+        got=>{
+            for(let promotion of got.builds){
+                syncPromotion(promotion,process,job,buildId);
+            }
+        }
+    );
+}
+
+function syncPromotions(job,buildId) {
+    var jobUrl = url.parse(job.url);
+    jenkinsGet(jobUrl.path + "promotion/api/json",
+        got=>{
+            for(let process of got.processes){
+                syncProcess(process,job,buildId);
+            }
+        }
+    );
+}
+
 function syncBuilds(job,onSync) {
     for (var i in job.builds) {
         var build = job.builds[i];
         var buildUrl = url.parse(build.url);
-        console.log("Found build " + build.number + " at " + build.url);
-        getJson(buildUrl.protocol, buildUrl.host, buildUrl.path + "api/json",
-            config.jenkins.username, config.jenkins.password,
+        jenkinsGet(buildUrl.path + "api/json",
             function (jenkinsBuildData) {
                 let culprit;
                 if(jenkinsBuildData.culprits && jenkinsBuildData.culprits.length > 0){
@@ -72,15 +125,16 @@ function syncBuilds(job,onSync) {
                 }
                 let buildData = {
                     "id": jenkinsBuildData.id,
-                    "project": job.name,
+                    "repoName": job.name,
                     "duration": jenkinsBuildData.duration,
-                    "timestamp": new Date(jenkinsBuildData.timestamp).toISOString(),
+                    "created_timestamp": new Date(jenkinsBuildData.timestamp).toISOString(),
                     "status": jenkinsBuildData.result,
-                    "cause": jenkinsBuildData.description,
+                    "description": jenkinsBuildData.description,
                     "url": jenkinsBuildData.url,
                     "user": culprit
                 };
-                write(config.elasticsearch.ciindex, job.name, buildData.id + "", buildData)
+                write(config.elasticsearch.index, "build", buildData.id, buildData);
+                syncPromotions(job,jenkinsBuildData.id);
         });
     }
     onSync();
@@ -91,9 +145,7 @@ function syncJenkinsJobs(jobs) {
         var job = jobs.shift();
         if (job) {
             var jobUrl = url.parse(job.url);
-            console.log("Found job " + job.name + " at " + job.url);
-            getJson(jobUrl.protocol, jobUrl.host, jobUrl.path + "api/json",
-                config.jenkins.username, config.jenkins.password,
+            jenkinsGet(jobUrl.path + "api/json",
                 function (data) {
                     syncBuilds(data, function () {
                         syncJenkinsJobs(jobs);
@@ -101,9 +153,7 @@ function syncJenkinsJobs(jobs) {
                 });
         }
     } else {
-        console.log("Taking our jobs");
-        getJson(config.jenkins.protocol, config.jenkins.host, config.jenkins.path + "/api/json",
-            config.jenkins.username, config.jenkins.password,
+        jenkinsGet("api/json",
             function (data) {
                 syncJenkinsJobs(data.jobs);
             });
@@ -135,9 +185,11 @@ function gitlabGet(apiPath, reader, pageIndex, pageSize) {
             'PRIVATE-TOKEN': config.gitlab.private_token
         }
     };
-    console.log("get " + options.url);
+    console.info("Calling Gitlab: " + options.url);
     request(options, function (error, response, body) {
-        if (response.statusCode < 300) {
+        if(error) {
+            console.error("Error getting data from Gitlab: " + error)
+        }  else if (response && response.statusCode < 300) {
             let got = JSON.parse(body);
             if(got && Object.keys(got).length > 0) {
                 reader(got);
@@ -152,7 +204,8 @@ function gitlabGet(apiPath, reader, pageIndex, pageSize) {
                 gitlabGet(apiPath, reader, nextPageIndex, pageSize);
             }
         } else {
-            console.log("Error getting data from Gitlab: " + options.url  + " (" + response.statusCode + ")")
+            let cause = response ? response.satusCode : "unknown cause";
+            console.error("Error getting data from Gitlab: " + options.url  + " (" + cause + ")")
         }
     });
 }
@@ -162,12 +215,12 @@ function syncGitlabCommits(project) {
         for(let commit of commits) {
             let data = {
                 "uid": commit.id,
-                "name": project.name,
+                "repoName": project.name,
                 "user": commit.author_email,
                 "description": commit.title,
                 "created_timestamp": commit.created_at
             };
-            write(config.elasticsearch.vcsindex,"commit",commit.uid,data);
+            write(config.elasticsearch.index,"commit",commit.uid,data);
         }
     }, 0);
 }
@@ -177,13 +230,13 @@ function syncGitlabProjects() {
         for(let project of projects) {
             let data = {
                 "id": project.id,
-                "name": project.name,
+                "repoName": project.name,
                 "description": project.description,
                 "archived": project.archived,
                 "created_timestamp": project.created_at,
                 "activity_timestamp": project.last_activity_at
             };
-            write(config.elasticsearch.vcsindex,"project",project.id,data);
+            write(config.elasticsearch.index,"repo",project.id,data);
             syncGitlabCommits(project);
         }
     });
@@ -226,18 +279,16 @@ function setupElasticsearch() {
 }
 
 function setupJenkins() {
-    let jurl = url.parse(config.jenkins.url);
-    if(jurl){
-        config.jenkins.protocol=jurl.protocol;
-        config.jenkins.host=jurl.host;
-        config.jenkins.path=jurl.path;
+    if(!config.jenkins.url.endsWith('/')){
+        config.jenkins.url+='/';
     }
+    readJenkinsQueue();
     queue(syncJenkinsJobs);
 }
 
 function setupGitlab() {
     if(!config.gitlab.url) {
-        console.log("No URL set for GitLab");
+        console.error("No URL set for GitLab");
     }
     if(!config.gitlab.url.endsWith("/")){
         config.gitlab.url += "/";
@@ -249,13 +300,12 @@ function setupGitlab() {
                 let got = JSON.parse(body);
                 config.gitlab.private_token = got.private_token;
                 if(config.gitlab.private_token) {
-                    console.log("Gitlab: Authenticated");
                     queue(syncGitlabProjects);
                 } else {
-                    console.log("Gitlab: Invalid authentication response");
+                    console.error("Gitlab: Invalid authentication response");
                 }
             } else {
-                console.log("Gitlab: setup error: " + response.statusCode);
+                console.error("Gitlab: setup error: " + response.statusCode);
             }
         }
     );
@@ -269,7 +319,7 @@ function loadConfig(configLocation) {
 function boot() {
     let configLocation = process.argv[2];
     if(!configLocation) {
-        console.log("usage: node mondego.js <config file location>")
+        console.error("usage: node mondego.js <config file location>")
     } else {
         loadConfig(configLocation);
         bus.on(moduleReadyEvent,()=>{
